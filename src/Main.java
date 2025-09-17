9bb21f94249b7cf58c19fc1c41ba121ea7ec3d99
Aiyanu <aiyanu1.00@gmail.com>
2025-09-17 21:39:08 +0100

    Variables tutorial

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //Variable
+        int age = 32;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+
+        String name = "Iyanu";
+        String pizza = "pizza";
+        String email = "phisher419@gmail.com";
+        String car = "Benz";
+
+        System.out.println(name);
+    }
+}
