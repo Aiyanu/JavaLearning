859044517daceb69fc4f6d11c1fe30205d7f7253
Aiyanu <aiyanu1.00@gmail.com>
2025-09-20 17:58:35 +0100

    Random Class

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
-import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-       // if statements
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter you name: ");
-        String name = sc.nextLine();
-        if (name.isEmpty()){
-            System.out.println("You didn't enter your name");
-        }else{
-            System.out.println("Hello, " + name);
-        }
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
+        //Random Class
+       Random rand = new Random();
 
-        if (age >= 18){
-            System.out.println("You are an adult!");
-        } else if (age < 0) {
-            System.out.println("You haven't been born yet");
-        } else{
-            System.out.println("You are a minor!");
-        }
+       int randInt1;
+       double randDouble;
+       boolean randBoolean;
 
-        sc.close();
+        randInt1 = rand.nextInt(1,7);
+        randDouble = rand.nextDouble();
+        randBoolean = rand.nextBoolean();
+
+        System.out.println(randInt1);
+        System.out.println(randDouble);
+        System.out.println(randBoolean);
     }
 }
