9a7b48a4c13eff6867b0c68d3994a9856d528ae7
Aiyanu <aiyanu1.00@gmail.com>
2025-09-25 20:11:53 +0100

    Generics

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Countdown timer program
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of seconds to countdown from: ");
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            int count = sc.nextInt();
+        //HashMap
 
-            @Override
-            public void run() {
-                System.out.println(count);
-                count--;
-                if (count < 0) {
-                    System.out.println("HAPPY NEW YEAR!");
-                    timer.cancel();
-                }
-            }
-        };
-        timer.scheduleAtFixedRate(task,0,1000);
+        HashMap<String,Double> map = new HashMap<>();
+
+        map.put("apple",0.5);
+        map.put("orange",1.0);
+        map.put("pear",2.0);
+
+//        map.remove("apple");
+        if(map.containsKey("apple")){
+            System.out.println(map.get("apple"));
+        }
+        else{
+            System.out.println("Key not found");
+        }
+
+        System.out.println(map.containsValue(1.0));
+        System.out.println(map.size());
+        System.out.println(map);
+        for(String key:map.keySet()){
+            System.out.println(key+" : ₦"+map.get(key));
+        }
+
     }
 }
