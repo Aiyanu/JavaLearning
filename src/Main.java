42cc8827ddaedd339ced5f917e7e3860964048fa
Aiyanu <aiyanu1.00@gmail.com>
2025-09-25 19:34:57 +0100

    Countdown timer app

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
+        //Countdown timer program
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of seconds to countdown from: ");
         Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            int count = 3;
+        TimerTask task = new TimerTask() {
+            int count = sc.nextInt();
+
             @Override
-            public void run(){
-                System.out.println("Hello!");
+            public void run() {
+                System.out.println(count);
                 count--;
-                if(count <= 0){
-                    System.out.println("TASK COMPLETED!");
+                if (count < 0) {
+                    System.out.println("HAPPY NEW YEAR!");
                     timer.cancel();
                 }
             }
-
         };
-
-        timer.schedule(timerTask, 0,1000);
-
-
+        timer.scheduleAtFixedRate(task,0,1000);
     }
 }
