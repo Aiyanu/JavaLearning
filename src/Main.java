2968f92b864a9142c5ff485adbdf97d436de9951
Aiyanu <aiyanu1.00@gmail.com>
2025-09-25 19:03:03 +0100

    Anonymous Classes

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,18 @@
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 public class Main {
     public static void main(String[] args) {
-        //(LocalDate,LocalTime,LocalDateTime,UTC timestamped)
-        LocalDate date = LocalDate.now();
-        System.out.println(date);
-        LocalTime time = LocalTime.now();
-        System.out.println(time);
-        LocalDateTime datetime = LocalDateTime.now();
-        System.out.println(datetime);
-        Instant instant = Instant.now();
-        System.out.println(instant);
+        Dog dog = new Dog();
+        TalkingDog talkingDog = new TalkingDog();
+        //Instead
+        Dog dog2 = new Dog(){
+            @Override
+            void speak(){
+                System.out.println("Scooby Doo says *Ruh Roh*");
+            }
+        };
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        System.out.println(datetime.format(formatter));
 
-        LocalDateTime datetime1 = LocalDateTime.of(2025,1,1,12,0,0);
-        LocalDateTime datetime2 = LocalDateTime.of(2025,1,1,0,0,0);
-
-        if(datetime1.isBefore(datetime2)){
-            System.out.println(datetime1+" is earlier than "+datetime2);
-        }
-        else if(datetime1.isAfter(datetime2)){
-            System.out.println(datetime1+" is later than "+datetime2);
-        }
-        else if(datetime1.isEqual(datetime2)){
-            System.out.println(datetime1+" is equal to "+datetime2);
-        }
+        dog.speak();
+        talkingDog.speak();
+        dog2.speak();
     }
 }
